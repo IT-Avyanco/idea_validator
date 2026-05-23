e208be91a299a9bcbf62f0b88e2730f0663e858d
Sakti <Sakti@AVYANCO.LOCAL>
2026-05-23 17:09:49 +0400

    lag fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${poppins.variable} h-full`}>
+    <html lang="en" className={`${poppins.variable} h-full`} suppressHydrationWarning>
       <body className="min-h-full flex flex-col font-poppins antialiased">
         {children}
       </body>
